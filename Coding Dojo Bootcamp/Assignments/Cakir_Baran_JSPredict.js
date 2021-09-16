4cda24fa9574885ef55f12bedd025d90a4c6bd8d
Baran CAKIR <88524146+Brnckr1053@users.noreply.github.com>
2021-09-16 00:47:28 -0400

    Add files via upload

---
+++ Coding Dojo Bootcamp/Assignments/Cakir_Baran_JSPredict.js
@@ -0,0 +1,9 @@
+//function myBirthYearFunc(){
+  //      console.log("I was born in " + 1980);
+    //}
+    // it doesn't show anything
+
+    //function myBirthYearFunc(birthYearInput){
+       //     console.log("I was born in " + birthYearInput);
+     //   }
+        // it doesn't show anything
