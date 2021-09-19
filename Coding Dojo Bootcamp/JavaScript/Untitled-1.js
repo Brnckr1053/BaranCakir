019c826b40f451b99a6c8e67552e53a23c946531
Baran CAKIR <88524146+Brnckr1053@users.noreply.github.com>
2021-09-19 18:03:52 -0400

    Add files via upload

---
+++ Coding Dojo Bootcamp/JavaScript/Untitled-1.js
@@ -0,0 +1,159 @@
+//var count = 5;
+//var message = "Hello World";
+//var likesJS = true;
+
+//console.log(message)
+
+
+//var count = 5;
+//count = "I'm now a string!";
+///console.log(count)
+
+
+//var product = 3.14159 + 2.71828; // 5.85987
+//console.log(product)
+
+
+//var name = "Marisa";
+//var message = "JS is actually the trickiest language";
+//console.log(name + " says " + message);
+
+//console.log("the number is" + (12 + 3));         // "the number is 123"
+//console.log(12 + 3 + " is a different number"); // "15 is a different number"
+
+//var total = 10;
+//total += 5; // equivalent to total = total + 5;
+//console.log(total)
+
+//var number = 5;
+//console.log(number < 3); // this will log `false`
+
+
+//var isSunny = false;
+//var isRaining = true;
+    
+//if(isSunny) {
+//   console.log("Wear sunscreen");
+//}
+    
+//if(isRaining) {
+//   console.log("Bring an umbrella");
+//}
+
+
+//var today = new Date();
+//if(today.getDay() == 1) {
+    //console.log("I hate Mondays!");
+//}
+    
+//if(today.getDay() != 1) {
+  //  console.log("Today is alright!"):
+////}
+
+//var today = new Date();
+//if(today.getDay() == 1) {
+  //  console.log("I hate Mondays!");
+//} else if(today.getDay() == 5) {
+// console.log("Friday? More like Fri-yay!");
+//} else {
+//   console.log("Today is alright!"):
+//}
+
+
+
+//var temperature = 60; // let's assume this is degrees Fahrenheit
+//var isRaining = false;
+    
+//if(temperature >= 50) {
+  //  if(!isRaining) {
+    //    console.log("This is a good day to go for a walk!");
+    //}
+//}
+
+
+//var temperature = 60; // let's assume this is degrees Fahrenheit
+//var isRaining = false;
+    
+//if(temperature >= 50 && !raining) {
+  //  console.log("This is a good day to go for a walk!");
+//}
+
+
+
+//var is5even = 5 % 2 == 0;
+//var is500even = 500 % 2 == 0;
+    
+//console.log(is5Even);   // false
+//console.log(is500Even); // true
+
+
+
+//for(var i=0; i<3; i++) {
+  //  console.log(i);
+//}
+
+
+//for(var i=10; i>0; i--) {
+  //  console.log(i);
+//}
+
+//for(var i=12; i>3; i-=2) {
+  //  console.log(i);
+//}
+// this will print 12, 10, 8, 6, 4
+    
+//for(var i=0.25; i<3; i+=0.5) {
+  //  console.log(i);
+//}
+// this will print 0.25, 0.75, 1.25, 1.75, 2.25, 2.75
+
+/*fsefse
+sefsefsef
+esfes
+*/
+
+//for(var i=0; i<3; i++) {
+  //  console.log(i);
+//}
+    
+//var i = 0;
+//while(i<3) {
+  //  console.log(i);
+  //  i++;
+// //}
+// sdfsdf
+// sdfsdfsdf
+
+// sdfsdfsdf
+
+
+// var x = "0";
+// for(var i=1; i<5; i++) {
+//     x += i;
+// }
+// console.log(x);
+
+
+
+for (i=1; i<16; i++){
+    
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz")
+    }
+    
+    
+    else if(i % 3 == 0) {
+        console.log("fizz")
+    }
+    else if(i % 5 == 0) {
+        console.log("buzz")
+    }
+   
+    else{
+        console.log(i)
+    }
+}
+
+
+
+
