131ced691cf77b197f3a7c09bf03975883a1fca6
Baran CAKIR <88524146+Brnckr1053@users.noreply.github.com>
2021-09-20 20:43:59 -0400

    Add files via upload

---
+++ Coding Dojo Bootcamp/JavaScript/AssignmentsJS/Likes/likes.js
@@ -0,0 +1,22 @@
+var increaseNeil = 9
+var increaseNichole = 12
+var increaseJim = 9
+
+function raiseLikesNeil(){
+    increaseNeil++;
+    document.querySelector("#number1").innerText = increaseNeil;
+}
+
+
+
+function raiseLikesNichole(){
+    increaseNichole++;
+    document.querySelector("#number2").innerText = increaseNichole;
+}
+
+
+
+function raiseLikesJim(){
+    increaseJim++;
+    document.querySelector("#number3").innerText = increaseJim;
+}
