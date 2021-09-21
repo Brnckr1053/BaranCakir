8ac3ad5039368f9ff7e326323f6cd8f996439f77
Baran CAKIR <88524146+Brnckr1053@users.noreply.github.com>
2021-09-21 02:27:36 -0400

    Add files via upload

---
+++ Coding Dojo Bootcamp/JavaScript/AssignmentsJS/Making Connections/making_connections.js
@@ -0,0 +1,61 @@
+var newname = "Pistachio H"
+
+function changeName(){
+    newname;
+    document.querySelector("#myname").innerText = newname;
+}
+
+console.log(newname)
+
+//
+var increaseReq = 2
+var increaseCon = 418
+function person1(){
+    increaseReq++;
+    increaseCon++;
+    document.querySelector(".radius2").innerText = increaseReq;
+    document.querySelector(".radius500").innerText = increaseCon;
+    var el = document.getElementById('todd');
+    el.remove()
+    
+}
+console.log(person1)
+
+
+
+//
+
+var increaseReq = 2
+var increaseCon = 418
+function person2(){
+    increaseReq++;
+    increaseCon++;
+    document.querySelector(".radius2").innerText = increaseReq;
+    document.querySelector(".radius500").innerText = increaseCon;
+    var el = document.getElementById('phil');
+    el.remove()
+    
+}
+console.log(person2)
+
+//
+
+function personnot1(){
+
+    var el = document.getElementById('todd');
+    el.remove()
+
+}
+
+//
+
+function personnot2(){
+
+    var el = document.getElementById('phil');
+    el.remove()
+
+}
+
+
+
+
