5aac6b36c8cc9d0b20c6eed2bb807a342e1af93a
Baran CAKIR <88524146+Brnckr1053@users.noreply.github.com>
2021-09-28 02:12:24 -0400

    Add files via upload

---
+++ Coding Dojo Bootcamp/Python/Assignments/For_Loop_Basics_1/for_loop_basic_1.py
@@ -0,0 +1,32 @@
+# for x in range(150):
+#     print(x)
+
+
+# for x in range(5, 1000, 5):
+#     print(x)
+
+# for x in range(1,101):
+#     if x % 10 == 0:
+#         print("codingdojo")
+#     elif x % 5 == 0:
+#         print("coding")
+#     else:
+#         print(x)
+
+# start, end = 0, 500000
+# oddsum = 0
+# for x in range(start, end + 1):
+#     if x % 2 != 0:
+#         oddsum += x
+# print(oddsum)
+
+
+
+# for x in range(2018, 0, -4):
+#     print(x)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(lowNum + 1, highNum + 1, mult):
+    print(x)
