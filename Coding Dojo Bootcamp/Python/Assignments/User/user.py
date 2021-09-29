81664e98143c74e0e01062f3dd788228be7bf190
Baran CAKIR <88524146+Brnckr1053@users.noreply.github.com>
2021-09-29 16:34:45 -0400

    Add files via upload

---
+++ Coding Dojo Bootcamp/Python/Assignments/User/user.py
@@ -0,0 +1,49 @@
+class user:
+
+    def __init__(self, name, email):
+        self.account_balance = 0
+        self.name = name
+        self.email = email
+
+    def display_user_balance(self):
+        print(f"Total money is {self.account_balance} $ in {self.name}'s acoount")
+        return self
+
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+        print(f"{amount} $ have taken by {self.name}")
+        self.display_user_balance()
+        return self
+
+    def make_deposit(self, amount):
+        self.account_balance += amount
+        print(f"{amount} $ have deposited by {self.name}")
+        self.display_user_balance()
+        return self
+
+    def transfer_money(self, user, amount):
+        print(f" Transferred {amount} $ from {self.name} to {user.name}")
+        self.make_withdrawal(amount)
+        user.make_deposit(amount)
+        return self
+
+guido = user("Guido van Rossum", "guido@abc.com") 
+baran = user("Baran Cakir", "baran10cakir53@gmail.com") 
+pistachio = user("Pistachio", "pistachio@cat.com")
+
+
+print(baran.name)
+
+print(" ")
+guido.make_deposit(250)
+
+print(" ")
+pistachio.make_deposit(1500)
+pistachio.make_withdrawal(500)
+
+print(" ")
+pistachio.display_user_balance
+
+
+print(" ")
+pistachio.transfer_money(baran, 250)
